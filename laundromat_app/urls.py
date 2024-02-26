efe0589529ad1ad6e226b7420868cb2c9dadd455
Matthew <matthewc54@outlook.com>
2024-02-26 15:32:10 -0700

    Push with urls combined with views

---
+++ laundromat_app/urls.py
@@ -0,0 +1,20 @@
+'''
+Contains urls for different app pages as well as the django admin site
+'''
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Django admin site
+    path('admin/', admin.site.urls),
+    # Laundromat listings page
+    path('listing/', views.laundromat_listing, name = 'laundromat_listing'),
+    # Default home page
+    path('', views.home_page, name = 'home_page'),
+    # Contact us page
+    path('contact/', views.contact_us, name = 'contact_us'),
+    # About page
+    path('about/', views.about, name = 'about'),
+]
--- laundromat_app/views.py
+++ laundromat_app/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import ContactForm
+from .models import Laundromat  # Make sure we already created Laundromat model in models.py
 
-# Create your views here.
+def laundromat_listing(request):
+    # Retrieve all laundromat objects from the database
+    laundromats = Laundromat.objects.all()
+    
+    # Pass the laundromats to the template
+    # The template will need to iterate over each laundromat and display its details
+    return render(request, 'laundromat_listing.html', {'laundromats': laundromats})
+
+
+#loads the home page view layout and infomation will be covered in template desgin
+def home_page(request):
+    return render(request, 'home.html')
+
+#checks for all 3 fields to be filled in, if it is it sends a thank you message, if not leaves the form stil up
+def contact_us(request):
+  if request.method == 'POST':
+    form = ContactForm(request.POST)
+    if form.is_valid():
+      name = form.cleaned_data['name']
+      email = form.cleaned_data['email']
+      message = form.cleaned_data['message']
+      return HttpResponse('/thank-you/')
+
+  else:
+    form = ContactForm()
+
+  return render(request, 'contact.html', {'form': form})
+
+def about(request):
+#the about page using the'about.html'template
+  return render(request, 'about.html')
+
+
+
