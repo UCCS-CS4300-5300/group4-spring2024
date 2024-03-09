24144d8beee67c13b783214b160e0b962b9dfb7f
Kacy Horning <khorning@uccs.edu>
2024-03-09 15:48:22 -0700

    removed duplicate Laundry and Contact forms

--- laundromat_app/forms.py
+++ laundromat_app/forms.py
@@ -14,19 +14,6 @@ class ContactForm(forms.Form):
     email = forms.EmailField(label='Your Email')
     message = forms.CharField(widget=forms.Textarea(attrs={'rows': 4, 'maxlength': '200'}), label='Leave a message for our team')
 
-
-
-#a form used for creating a laundromat in the app
-class LaundromatForm(forms.ModelForm):
-    class Meta:
-        model = Laundromat
-        fields = ['name', 'location']
-
-class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, label='Your Name')
-    email = forms.EmailField(label='Your Email')
-    message = forms.CharField(widget=forms.Textarea(attrs={'rows': 4, 'maxlength': '200'}), label='Leave a message for our team')
-
 # for creating/editing associated laundromat (NOT RESVERATION!)
 class MachineForm(forms.ModelForm):
     class Meta:
