580af559ef8d5de84236c71e79fab69b8763e09e
Jrod0703 <98058730+Jrod0703@users.noreply.github.com>
2024-03-12 12:11:52 -0600

    Update tests.py
    Final task for sprint 1

--- laundromat_app/tests.py
+++ laundromat_app/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Machines, Laundromat
 
-# Create your tests here.
+#creating a fake test laundromat doesn't go into DB
+def setUp(self):
+    self.laundromat = Laundromat.objects.create(name="Test Laundromat")
+
+
+#  creating a machine instance and saving it to the DB
+def create_machine(self):
+    machine = Machines.objects.create(
+        laundromat=self.laundromat,
+        machine_ID="1",
+        machine_choice=Machines.Dryer,
+        status=Machines.Open
+        )
+    self.assertIsInstance(machine, Machines)
+
+#testing deafult machine availability
+def machine_default_availability (self):
+    machine = Machines.objects.create(
+        laundromat=self.laundromat,
+        machine_ID="1",
+        machine_choice=Machines.Dryer
+    )
+    self.assertEqual(machine.status, Machines.Open)
