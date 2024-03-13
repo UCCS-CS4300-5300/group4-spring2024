3fe48b7b9c35dc419870e4739c5ecb2affb02f19
Matthew_Cao <matthewc54@outlook.com>
2024-03-12 19:23:24 -0600

    ok

--- laundromat_app/tests_laundromat_model.py
+++ laundromat_app/tests_laundromat_model.py
@@ -1,14 +1,32 @@
 from django.test import TestCase
 from .models import Machines, Laundromat
+from django.urls import reverse
 
 
-class LaundromatTest(TestCase):
-    
+class LaundromatTest(TestCase):    
     #creating a fake test laundromat doesn't go into DB
     def setUp(self):
         self.laundromat = Laundromat.objects.create(name="Test Laundromat")
 
-
+        # Set up a new user to test inputs
+        self.home_page = Laundromat.objects.create(name = 'Bob Smith', location = 'Hawaii', hours = 'Mon-Fri 24/7', \
+            description = 'very cool laundry place')
+        
+    # Test app urls
+    def test_app_urls(self):
+        # Access home page
+        home_url = reverse('home_page')
+        response = self.client.get(home_url)
+        
+        self.assertEqual(response.status_code, 200)
+            
+    # Test user data inputs
+    def test_new_user(self):
+        self.assertEqual(self.home_page.name, 'Bob Smith')
+        self.assertEqual(self.home_page.location, 'Hawaii')
+        self.assertEqual(self.home_page.hours, 'Mon-Fri 24/7')
+        self.assertEqual(self.home_page.description, 'very cool laundry place')    
+    
     #  creating a machine instance and saving it to the DB
     def test_create_machine(self):
         machine = Machines.objects.create(
