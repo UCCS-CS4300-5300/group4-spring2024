c34912567c7dea74dab3ee54d52688f296f652d4
Matthew_Cao <matthewc54@outlook.com>
2024-03-12 18:43:44 -0600

    latest push

--- laundromat_app/models.py
+++ laundromat_app/models.py
@@ -6,23 +6,9 @@ from django.urls import reverse
 class Laundromat(models.Model):
     name = models.CharField(max_length = 100)
     location = models.CharField(max_length = 100)
+    hours = models.CharField(max_length = 100)
+    description = models.CharField(max_length = 500)
     
-   
-
-    def __str__(self):
-        return self.name
-    
-    def get_absolute_url(self):
-        return reverse('laundromat_detail', args=[str(self.id)])
-    
-
-#redoing hopefully it will push ( justin).
-
-class Laundromat(models.Model):
-    name = models.CharField(max_length = 100)
-    location = models.CharField(max_length = 100)
-   
-
     def __str__(self):
         return self.name
     
