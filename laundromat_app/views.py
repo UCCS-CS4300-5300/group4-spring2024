02c96a185689662f952eb01d89fa39b6b8a213cc
Jrod0703 <98058730+Jrod0703@users.noreply.github.com>
2024-03-30 13:05:48 -0600

    Update views.py

--- laundromat_app/views.py
+++ laundromat_app/views.py
@@ -3,19 +3,50 @@ from django.shortcuts import render
 from django.views import generic
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.http import HttpResponse
-from .forms import ContactForm, LaundromatForm
-from .models import Laundromat  # Make sure we already created Laundromat model in models.py
+from .forms import ContactForm, LaundromatForm, MachineForm
+from .models import Laundromat, Machines  # Make sure we already created Laundromat model in models.py
 from django.urls import reverse_lazy, reverse
 from django.shortcuts import get_object_or_404
+from django.conf import settings  # New import
+
+from django.shortcuts import render
+from django.conf import settings
+import requests
 
 def laundromat_listing(request):
-    # Retrieve all laundromat objects from the database
-    laundromats = Laundromat.objects.all()
+    # user's search query (city or zip code)
+    search_query = request.GET.get('q', '')
     
-    # Pass the laundromats to the template
-    # The template will need to iterate over each laundromat and display its details
-    return render(request, 'laundromat_listing.html', {'laundromats': laundromats})
+    # list to hold the laundromat data
+    laundromats = []
 
+    if search_query:
+        #  (make sure to set it in your environment or settings file... I did both)
+        api_key = settings.GOOGLE_API_KEY
+
+        # building the Geocoding API request URL
+        geocode_url = f"https://maps.googleapis.com/maps/api/geocode/json?address={search_query}&key={api_key}"
+        
+        # requesting to the Geocoding API
+        geocode_response = requests.get(geocode_url).json()
+
+        if geocode_response.get('status') == 'OK':
+            # taking the latitude and longitude from the Geocoding API response
+            location = geocode_response['results'][0]['geometry']['location']
+            latitude, longitude = location['lat'], location['lng']
+
+            # creating the Places API request URL
+            places_url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={latitude},{longitude}&radius=5000&type=laundry&key={api_key}"
+            
+            # request to the Places API
+            places_response = requests.get(places_url).json()
+
+            if places_response.get('status') == 'OK':
+                # takes the places API response data to the laundromats list
+                laundromats = places_response['results']
+
+    # Render the template with the list of laundromats from above
+    return render(request, 'laundromat_list.html', {'laundromat_list': laundromats})
 
 #loads the home page view layout and infomation will be covered in template desgin
 def home_page(request):
@@ -73,6 +104,7 @@ class LaundromatUpdate(UpdateView):
 
     def form_valid(self, form):
         # Save the form data to the database
+        form.save()
         return super().form_valid(form)
 
 class LaundromatDeleteView(DeleteView):
@@ -91,4 +123,135 @@ class LaundromatListView(generic.ListView):
 class LaundromatDetailView(generic.DetailView):
    model = Laundromat
    template_name = 'laundromat_detail.html'
+
+#view for the machine creation page
+class MachineCreate(CreateView):
+  model = Machines
+  form_class = MachineForm
+  template_name = 'machine_form.html'
+  
+  def get_success_url(self):
+    return reverse('machine_list', kwargs={'pk': self.object.laundromat.pk})
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    # Get the laundromat object based on the URL parameter
+    laundromat_id = self.kwargs.get('pk')
+    laundromat = get_object_or_404(Laundromat, pk=laundromat_id)
+    # Pass the laundromat name to the template context
+    context['laundromat'] = laundromat
+    return context
+
+  def form_valid(self, form):
+    # Get the laundromat object based on the URL parameter
+    laundromat_id = self.kwargs.get('pk')
+    laundromat = get_object_or_404(Laundromat, pk=laundromat_id)
+    
+    # Set the laundromat field in the form instance
+    form.instance.laundromat = laundromat
+    form.instance.status = 'Open'
+    
+    # Call the parent class's form_valid method to save the form data
+    return super().form_valid(form)
+  
+  #for debugging the form not being saved
+  def form_invalid(self, form):
+    print(form.errors)
+    response = super().form_invalid(form)
+    return response
+    
+class MachineUpdate(UpdateView):
+  model = Machines
+  form_class = MachineForm
+  template_name = 'machine_update.html'
+
+  def get_object(self, queryset=None):
+    # Retrieve the specific machine object to be updated
+    laundromat_id = self.kwargs.get('laundromat_pk')
+    machine_id = self.kwargs.get('pk')
+    machine = get_object_or_404(Machines, laundromat_id=laundromat_id, pk=machine_id)
+    return machine
+
+  def get_initial(self):
+    # Provide initial data for the form
+    initial = super().get_initial()
+    machine = self.get_object()
+    # Populate the form fields with the instance's current values
+    initial['laundromat'] = machine.laundromat
+    initial['machine_ID'] = machine.machine_ID
+    initial['machine_choice'] = machine.machine_choice
+    initial['status'] = machine.status
+    return initial
+
+  def get_success_url(self):
+    # Redirect to the machine list page after successful update
+    return reverse('machine_list', kwargs={'pk': self.object.laundromat.pk})
+  
+  def form_valid(self, form):
+    # Save the form data to the database
+    form.save()
+    return super().form_valid(form)
+  
+    #for debugging the form not being saved
+  def form_invalid(self, form):
+    print(form.errors)
+    response = super().form_invalid(form)
+    return response
+
+
+class MachineDeleteView(DeleteView):
+    model = Machines
+    # Template for confirmation page
+    template_name = 'machine_confirm_delete.html' 
+
+    def get_object(self, queryset=None):
+      # Retrieve the specific machine object to be updated
+      laundromat_id = self.kwargs.get('laundromat_pk')
+      machine_id = self.kwargs.get('pk')
+      machine = get_object_or_404(Machines, laundromat_id=laundromat_id, pk=machine_id)
+      return machine 
+    
+    def get_success_url(self):
+      return reverse_lazy('machine_list', kwargs={'pk': self.kwargs['laundromat_pk']})
+
+class MachineListView(generic.ListView):
+   model = Machines
+   template_name = 'machine_list.html'
+
+   def get_queryset(self):
+       # Get the laundromat object based on the URL parameter
+       laundromat_id = self.kwargs.get('pk')
+       laundromat = get_object_or_404(Laundromat, pk=laundromat_id)
+       
+       # Filter machines based on the laundromat
+       queryset = Machines.objects.filter(laundromat=laundromat)
+       return queryset
+
+   def get_context_data(self, **kwargs):
+      context = super().get_context_data(**kwargs)
+      # Get the laundromat object based on the URL parameter
+      laundromat_id = self.kwargs.get('pk')
+      laundromat = get_object_or_404(Laundromat, pk=laundromat_id)
+      # Pass the laundromat name to the template context
+      context['laundromat'] = laundromat
+
+      # Add the machine_list queryset to the context
+      context['machine_list'] = self.get_queryset()
+      return context
+
+class MachineDetailView(generic.DetailView):
+  model = Machines
+  template_name = 'machine_detail.html'
+  context_object_name = 'machine'
+
+  def get_object(self, queryset=None):
+    # Get the primary key of the machine from the URL
+    machine_pk = self.kwargs.get('pk')
+    # Get the primary key of the laundromat from the URL
+    laundromat_pk = self.kwargs.get('laundromat_pk')
+    # Query the Machines model to get the specific machine
+    # Filter by both machine_pk and laundromat_pk to ensure correct machine retrieval
+    machine = Machines.objects.filter(pk=machine_pk, laundromat_id=laundromat_pk).first()
+    return machine
+
 
