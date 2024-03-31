44a88f6397567d07d7112efd9b6fd30ac59e1988
plethorapanther <97998646+plethorapanther@users.noreply.github.com>
2024-03-30 19:17:07 -0600

    Revert "Logouttests"

--- laundromat_app/custom_tags.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name='is_owner')
-def is_owner(user):
-    return user.groups.filter(name='Owner').exists()
--- laundromat_app/models.py
+++ laundromat_app/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Laundromat(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='laundromats', null=True)
     name = models.CharField(max_length = 100)
     location = models.CharField(max_length = 100)
     hours = models.CharField(max_length = 100)
